edabca033efcf52d90ae2b94c9ef47ce53a04cbf
mrautenberg <max.rautenberg89@gmail.com>
2021-01-29 12:00:21 +0100

    useState added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import { useEffect, useState } from 'react'
+
 const App = () => {
+
+  // vi kan ha hårdkodade världen eller
+  // olika datatyper som vi vill initiera
+  const [login, setLogin] = useState(null);
+
+  const API_URL = "https://api.github.com/users/patrik-fredlund"
+
+  // useEffect(() => {
+  //   fetch(API_URL)
+  //     .then(res => res.json())
+  //     .then(data => setLogin(data.login))
+  // }, [])
+
+  useEffect(() => {
+    async function fetchApi(url) {
+      const res = await fetch(url);
+      const data = await res.json();
+      setLogin(data.login);
+    }
+    fetchApi(API_URL)
+  }, [])
+
 
   return (
     <div>
-
+      our state is: {login}
     </div>
   );
 }
